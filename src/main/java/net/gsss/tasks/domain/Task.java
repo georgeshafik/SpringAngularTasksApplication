71684951e4190cf76bb25b74640fe52aaa266e1f
George Shafik <george@gsss.net>
2019-08-27 18:49:29 +1000

    Spring Boot Web - Create our domain object Task. Closed #1

--- src/main/java/net/gsss/tasks/domain/Task.java
+++ src/main/java/net/gsss/tasks/domain/Task.java
@@ -1,7 +1,29 @@
 package net.gsss.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
 /**
  * Created by gs on 2019-08-27
  */
+@Entity
+@Data
+@AllArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private LocalDate dueDate;
+    private Boolean completed;
 }
